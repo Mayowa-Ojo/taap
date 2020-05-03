3976290d6dad53167f337fa2cde601e10b52232f
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-02 23:29:35 -0600

    feat: render Navbar and Task components

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,20 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import Wrapper from "./components/wrapper";
+import Task from "./containers/task/task";
+import Navbar from "./components/navbar";
 import "./popup.css";
 
-function App() {
-  const [counter, setCounter] = React.useState(0)
-
-  const handleClick = () => {
-    setCounter(counter + 1)
-  }
+function App() {  
 
   return (
     <Wrapper>
-      <main>
-        <h1>Click to increase counter: <strong>{counter}</strong></h1>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>Increment Counter +</button>
+      <main className="flex flex-col w-full">
+        <Navbar />
+        <Task />
       </main>
     </Wrapper>
   )
