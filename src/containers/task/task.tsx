b118409999f55b7c4c5f88b8f1080890023644b1
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-04 00:22:03 -0600

    feat: implement card component

--- src/containers/task/task.tsx
+++ src/containers/task/task.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 
+import Card from "./components/card";
+import "./task.css";
+
 export default function task() {
    const [counter, setCounter] = React.useState(0)
 
@@ -8,10 +11,45 @@ export default function task() {
    }
    
    return (
-      <div className="wrapper h-full flex flex-row">
-         <div className="h-full"></div>
-         <div className="h-full"></div>
-         <div className="h-full"></div>
+      <div className="wrapper overflow-y-scroll h-full flex flex-row pt-8">
+         <div className="flex flex-col items-center py-4 w-1/3 border-r border-solid border-gray-300 h-full">
+            <h4 className="flex items-center text-gray-700">
+               <svg className="mr-2" width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="3" cy="3" r="3" fill="#FFC13D"/>
+               </svg>
+               In-progress
+            </h4>
+            <Card />
+            <Card />
+            <Card />
+         </div>
+         <div className="flex flex-col items-center py-4 w-1/3 border-r border-solid border-gray-300 h-full">
+            <h4 className="flex items-center text-gray-700">
+               <svg className="mr-2" width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="3" cy="3" r="3" fill="#45F3D4"/>
+               </svg>
+               Completed
+            </h4>
+            <Card />
+            <Card />
+            <Card />
+         </div>
+         <div className="flex flex-col items-center py-4 w-1/3 border-r border-solid border-gray-300 h-full">
+            <h4 className="flex items-center text-gray-700">
+               <svg className="mr-2" width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="3" cy="3" r="3" fill="#ED483D"/>
+               </svg>
+               Unfulfilled
+            </h4>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+         </div>
       </div>
    )
 }
