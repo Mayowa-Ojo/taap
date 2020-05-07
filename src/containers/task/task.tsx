117e1b9513b41488ae49a0bbc415528fc9adbbfa
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-06 23:23:15 -0600

    refactor: remove bleeding borders

--- src/containers/task/task.tsx
+++ src/containers/task/task.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import Card from "./components/card/card";
+import { insertMany } from "../../database/taskdb";
 import "./task.css";
 
 export default function Task() {
@@ -9,10 +10,16 @@ export default function Task() {
    const handleClick = () => {
       setCounter(counter + 1)
    }
+
+   //React.useEffect(() => {
+   //   (async () => {
+   //      await insertMany()
+   //   })()
+   //}, [])
    
    return (
       <div className="wrapper overflow-y-scroll h-full flex flex-row pt-8">
-         <div className="flex flex-col items-center py-4 w-1/3 border-r border-solid border-gray-300 h-full">
+         <div className="flex flex-col items-center py-4 w-1/3 h-full">
             <h4 className="flex items-center text-gray-700">
                <svg className="mr-2" width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <circle cx="3" cy="3" r="3" fill="#FFC13D"/>
@@ -23,7 +30,7 @@ export default function Task() {
             <Card />
             <Card />
          </div>
-         <div className="flex flex-col items-center py-4 w-1/3 border-r border-solid border-gray-300 h-full">
+         <div className="flex flex-col items-center py-4 w-1/3 border-r border-l border-solid border-gray-300 h-full">
             <h4 className="flex items-center text-gray-700">
                <svg className="mr-2" width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <circle cx="3" cy="3" r="3" fill="#45F3D4"/>
@@ -34,7 +41,7 @@ export default function Task() {
             <Card />
             <Card />
          </div>
-         <div className="flex flex-col items-center py-4 w-1/3 border-r border-solid border-gray-300 h-full">
+         <div className="flex flex-col items-center py-4 w-1/3 h-full">
             <h4 className="flex items-center text-gray-700">
                <svg className="mr-2" width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <circle cx="3" cy="3" r="3" fill="#ED483D"/>
