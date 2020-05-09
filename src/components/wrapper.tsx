6178620df64cbebb9fe190ea11999e0e395470c5
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-09 09:39:55 -0600

    feat: add type annotations

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
 
-const Wrapper: React.FC = (props) => {
+const Wrapper: React.FC<WrapperProp> = (props) => {
    return (
       <div className="flex flex-row w-full h-full" >
          <Sidebar />
