299a6a302a2f27b85631c8674df0470d65b0fb11
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-09 09:40:48 -0600

    feat: add seed data

---
+++ src/database/seed.ts
@@ -0,0 +1,16 @@
+export const tasks_seed: Task[] = [
+   { id: 1, deet: "Design UI templates for next project", status: "in-progress", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Write article on alpinejs and dexie", status: "in-progress", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Finish chrome extension", status: "completed", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Create UI design for vendor pages", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "completed", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "in-progress", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "completed", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "unfulfilled", createdAt: new Date(), due: new Date(), priority: 1},
+   { id: 1, deet: "Complete alorithms course", status: "in-progress", createdAt: new Date(), due: new Date(), priority: 1},
+]
