6f7684a5ccc02476ca4de1c15223994fc98fef6b
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-09 22:06:00 -0600

    feat: define database handlers

---
+++ src/database/taskdb.ts
@@ -0,0 +1,97 @@
+import db from "./indexdb";
+import utils from "../utils/utils";
+
+export async function findOne(id: number): Promise<any> {
+   try {
+      //...
+      const response = await db.table("tasks").where("id").equals(id)
+
+      return response
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function findAll(): Promise<Task[]> {
+   try {
+      //...
+      const response = await db.table("tasks").toArray()
+
+      return response
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function findMany(query: Status): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function insertOne(data: Task): Promise<void> {
+   try {
+      await db.table("tasks").put(data)
+   } catch(err) {
+      console.error(err)
+   }
+}
+
+export async function insertMany(data: Task[]): Promise<void> {
+   const parsedData = utils.injectID<Task>(data)
+   try {
+      await db.table("tasks").bulkPut(parsedData)
+   } catch(err) {
+      console.error(err)
+   }
+}
+
+export async function updateOne(): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function updateMany(): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function updateAll(): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function deleteOne(): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function deleteMany(): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
+
+export async function deleteAll(): Promise<void> {
+   try {
+      //...
+   } catch (err) {
+      console.error(err)
+   }
+}
