dd4340c6e112602ce512554cb09632f9ba82fe65
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-11 10:18:33 -0600

    feat: define component prop types

--- src/containers/types.d.ts
+++ src/containers/types.d.ts
@@ -10,3 +10,11 @@ declare type WrapperProp = {
 declare type CardProp = {
    task: Task
 }
+
+declare type FormProp = {
+   toggleModal: () => void
+}
+
+declare type NavbarProp = {
+   toggleModal: () => void
+}
