80413fc20af6bdec3fcb72df0eca5ecbe82e671b
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-11 10:17:54 -0600

    feat: add state for modal component

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,16 +3,23 @@ import * as ReactDOM from "react-dom";
 
 import Wrapper from "./components/wrapper";
 import Task from "./containers/task/task";
+import Form from "./containers/task/components/form/form";
 import Navbar from "./components/navbar";
 import "./popup.css";
 
 function App() {  
+  const [isOpen, setIsOpen] = React.useState<boolean>(false)
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <Wrapper>
       <main className="flex flex-col w-full">
-        <Navbar />
+        <Navbar toggleModal={toggleModal} />
         <Task />
+        { isOpen ? <Form toggleModal={toggleModal} /> : ""}
       </main>
     </Wrapper>
   )
