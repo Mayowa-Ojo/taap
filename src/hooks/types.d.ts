ed36ea8560a0e4c1ca2643358b43f62df3158b05
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-22 23:34:43 -0600

    refactor: update type declaration

--- src/hooks/types.d.ts
+++ src/hooks/types.d.ts
@@ -11,5 +11,5 @@ declare type EventElement = HTMLInputElement | HTMLTextAreaElement
 declare type Params = {
    initialValues: FormValues
    persistFormData: (values: FormValues) => void
-   toggleModal: () => void
+   toggleModal: ToggleModal
 }
