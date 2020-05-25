30a79a8873d56b57e210cebaf598bdf1e5f17ecc
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-24 23:42:31 -0600

    feat: create sub components

---
+++ src/containers/password_keeper/components/illustration/illustration.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Illustration = () => {
+   return (
+      <div className="w-1/2 h-64 bg-white mx-4 mt-4">
+         Illustration Component
+      </div>
+   )
+}
+
+export default Illustration
---
+++ src/containers/password_keeper/components/locker/locker.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Locker = () => {
+   return (
+      <div className="w-1/2 h-72 bg-white mx-4">
+         Locker Component
+      </div>
+   )
+}
+
+export default Locker
