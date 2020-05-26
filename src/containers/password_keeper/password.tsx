e4281555b5f8232d90ee9f3c2ad968e7bf44fe2d
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-26 17:00:45 -0600

    feat: implement UI elements

--- src/containers/password_keeper/password.tsx
+++ src/containers/password_keeper/password.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
 
+import Locker from "./components/locker/locker";
+import Illustration from "./components/illustration/illustration";
+import "./password.scss";
+
 const Password = () => {
    return (
-      <div>
-         Password Keeper
+      <div className="h-full flex justify-center px-16 pt-16">
+         <Locker />
+         <Illustration />
       </div>
    )
 }
