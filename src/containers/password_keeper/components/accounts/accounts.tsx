b91f05118b0b0bdb135d6cb056bb171afef292cf
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-27 23:56:20 -0600

    feat: create sub components

---
+++ src/containers/password_keeper/components/accounts/accounts.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+
+import Account from "../account/account";
+import "./accounts.scss";
+
+const Accounts = () => {
+   return (
+      <div className="accounts w-1/2 h-80 bg-white mx-4 shadow overflow-auto">
+         <div className="header flex justify-between px-4 my-2">
+            <span className="display-none"></span>
+            <h1 className="text-lg text-indigo-500">credentials</h1>
+            <svg className="h-4 w-4 mt-2 cursor-pointer" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path d="M10 6.11111V8.88889C10 9.18357 9.88294 9.46619 9.67456 9.67456C9.46619 9.88294 9.18357 10 8.88889 10H1.11111C0.816426 10 0.533811 9.88294 0.325437 9.67456C0.117063 9.46619 0 9.18357 0 8.88889V1.11111C0 0.816426 0.117063 0.533811 0.325437 0.325437C0.533811 0.117063 0.816426 0 1.11111 0H3.88889V1.11111H1.11111V8.88889H8.88889V6.11111H10Z" fill="#6682E3"/>
+               <path d="M10 2.22222H7.77778V0H6.66667V2.22222H4.44444V3.33333H6.66667V5.55556H7.77778V3.33333H10" fill="#6682E3"/>
+            </svg>
+         </div>
+
+         <div className="accounts-list pl-4">
+            <Account />
+            <Account />
+            <Account />
+            <Account />
+            <Account />
+         </div>
+      </div>
+   )
+}
+
+export default Accounts
